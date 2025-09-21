c1b1d98b8003116802f6aca507e461923c68c0b0
Kidy0938 <143276202+Kidy0938@users.noreply.github.com>
2025-09-21 04:44:34 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import MiningSimulator from "../components/MiningSimulator";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 style={{textAlign:'center'}}>BTC Digger</h1>
+      <MiningSimulator />
+    </div>
+  );
+}
